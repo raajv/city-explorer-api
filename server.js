49e42981dd34cc5087ee319bdda5117f9a1251c0
raajv <raaj.vardhan@gmail.com>
2021-11-05 05:13:04 +0530

    added movie search with city

--- server.js
+++ server.js
@@ -7,18 +7,29 @@ const app = express();
 app.use(cors());
 
 app.get('/weather', handleGetWeather);
+app.get('/movie',handleGetMovie);
 
 async function handleGetWeather(req,res){
   const { lat, lon } = req.query;
   const url = `http://api.weatherbit.io/v2.0/current?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`
   try{const resultsFromAPI = await axios.get(url);
     const dailyWeather = resultsFromAPI.data.data.map(weath => new Weatherforecast(weath));
-    
     res.status(200).send(dailyWeather);
-    
 }catch(e){
   res.status(400);
 }}
+
+async function handleGetMovie(req,res){
+  const { query } = req.query;
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${query}`
+  try{const resultsFromAPI = await axios.get(url);
+    const searchedMovie = resultsFromAPI.data.results.map(movie=> new Moviesearched(movie));
+    console.log(searchedMovie);
+    res.status(200).send(searchedMovie);
+}catch(e){
+  res.status(400);
+}}
+
 
 
 const PORT = process.env.PORT 
@@ -33,3 +44,11 @@ class Weatherforecast {
   }
 }
 
+class Moviesearched {
+  constructor(obj){
+    this.original_title= obj.original_title;
+    this.overview=obj.overview;
+    this.release_date = obj.release_date;
+    this.poster_path = obj.poster_path;
+  }
+}
