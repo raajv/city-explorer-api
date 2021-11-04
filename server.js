e8266322f116d2901469712ec0752f0b516dea86
raajv <raaj.vardhan@gmail.com>
2021-11-04 17:21:32 +0530

    added weather api

--- server.js
+++ server.js
@@ -3,38 +3,33 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-
-// const shoppingList = require('./myShoppingList.json');
-const weather = require('./data/weather.json');
 const app = express();
 app.use(cors());
 
 app.get('/weather', handleGetWeather);
 
-function handleGetWeather(req,res){
-  const cityName = req.query.city;
-  const lat = req.query.lat;
-  const lon = req.query.lon;
-  const cityToSend =weather.find(city=>{
-    if((city.city_name === cityName)){
-      return true;
-    } return false;
-  });
-  if(cityToSend){
-    const forecastData =cityToSend.data.map(city=> new Weatherforecast(city));
-res.send(forecastData);
-}else{
-  res.send.status(404);
-}
-}
+async function handleGetWeather(req,res){
+  const { lat, lon } = req.query;
+  const url = `http://api.weatherbit.io/v2.0/current?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`
+  try{const resultsFromAPI = await axios.get(url);
+    const dailyWeather = resultsFromAPI.data.data.map(weath => new Weatherforecast(weath));
+    
+    res.status(200).send(dailyWeather);
+    
+}catch(e){
+  res.status(400);
+}}
+
+
 const PORT = process.env.PORT 
 app.get('/hello', (request, response) => { response.send('Hello, it works!') })
 app.listen(PORT, () => console.log(`I am a server that is listening on port:${PORT}`));
 
 class Weatherforecast {
   constructor(obj){
-    this.min_temp= obj.min_temp;
-    this.max_temp=obj.max_temp;
+    this.timezone= obj.timezone;
+    this.temp=obj.temp;
     this.description = obj.weather.description;
   }
 }
+
