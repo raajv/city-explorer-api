30e5dd6a6e473cdb3e2e0378e06dccc8edbeb169
raajv <raaj.vardhan@gmail.com>
2021-11-08 17:12:29 +0530

    cahnges to include cache for wx and mov using startr code

---
+++ cachemov.js
@@ -0,0 +1,4 @@
+'use strict';
+const cachemov = {};
+
+module.exports = cachemov;
--- server.js
+++ server.js
@@ -2,17 +2,16 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const axios = require('axios');
 const app = express();
-const handleGetWeather = require('./weather.js')
+const weatherHandler = require('./weather.js')
 const handleGetMovie = require ('./movie.js')
 
 app.use(cors());
-app.get('/weather',handleGetWeather );
+app.get('/weather',weatherHandler );
 app.get('/movie',handleGetMovie);
 
 
 const PORT = process.env.PORT 
 app.get('/hello', (request, response) => { response.send('Hello, it works!') })
-app.listen(PORT, () => console.log(`I am a server that is listening on port:${PORT}`));
+app.listen(PORT, () => console.log(`Server up on ${PORT}`));
 
